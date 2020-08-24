d8bd182a835f5c97d0aef04025f89bb790edb43f
jamesj <ehddnr177@naver.com>
2020-08-24 10:31:20 +0900

    Modify codes

--- main.c
+++ main.c
@@ -52,24 +52,33 @@ struct input_data_s{
  * @return 문자열이 정수인지 검사 여부 반환. 성공 시 SUCCESS, 실패 시 FAIL 반환
  */
 int check_buffer( const char *buf){
-	int return_value = SUCCESS;	/** 검사 성공 여부를 설정할 변수 */
+	/** 검사 성공 여부를 설정할 변수 */
+	int return_value = SUCCESS;
 
-	if( buf == NULL){ /** 매개변수로 전달 받은 문자열 변수가 NULL 인 경우, return_value 에 FAIL 을 설정한다. */
+	/** 매개변수로 전달 받은 문자열 변수가 NULL 인 경우, return_value 에 FAIL 을 설정한다. */
+	if( buf == NULL){
 		printf("\t| ! [DEBUG] 버퍼 NULL 오류 발생\n");
 		return FAIL;
 	}
 
-	int minus_count = 0;				/** 음수 기호 카운트 변수 */
-	size_t loop_index = 0;				/** 반복문 인덱스 변수 */
-	size_t buf_size = strlen( buf);		/** 전달 받은 문자열의 길이 변수 */
+	/** 음수 기호 카운트 변수 */
+	int minus_count = 0;
+	/** 반복문 인덱스 변수 */
+	size_t loop_index = 0;
+	/** 전달 받은 문자열의 길이 변수 */
+	size_t buf_size = strlen( buf);
 
-	if( buf[0] == '\n'){ /** 아무것도 입력하지 않고, 엔터만 입력 시 return_value 에 FAIL 을 설정한다. 재입력 진행. */
+	/** 아무것도 입력하지 않고, 엔터만 입력 시 return_value 에 FAIL 을 설정한다. 재입력 진행. */
+	if( buf[0] == '\n'){
 		return AGAIN;
 	}
 
+
 	for( ; loop_index < buf_size; loop_index++){
-		if( buf[loop_index] == '\n') break; /** 해당 문자가 개행 문자일 경우, 해당 문자열의 끝이므로 반복문 종료 (by fgets function) */
-		if( buf[loop_index] == '-'){ /** 해당 문자가 '-'일 경우, 음수를 뜻하므로 계속 진행 */
+		/** 해당 문자가 개행 문자일 경우, 해당 문자열의 끝이므로 반복문 종료 (by fgets function) */
+		if( buf[loop_index] == '\n') break;
+		/** 해당 문자가 '-'일 경우, 음수를 뜻하므로 계속 진행 */
+		if( buf[loop_index] == '-'){
 			minus_count++;
 			/** 아래의 경우에 해당되면 잘못된 정수이므로 return_value 에 FAIL 을 설정한다. */
 			/** 1) '-' 문자가 숫자 사이에 있는 경우 */
@@ -86,7 +95,8 @@ int check_buffer( const char *buf){
 			}
 			continue;
 		}
-		if( isdigit( buf[loop_index]) == 0) { /** 해당 문자가 정수가 아닌 경우, isdigit 함수가 0 을 반환 */
+		/** 해당 문자가 정수가 아닌 경우, isdigit 함수가 0 을 반환 */
+		if( isdigit( buf[loop_index]) == 0) {
 			return_value = FAIL;
 			break;
 		}
@@ -126,43 +136,51 @@ int convert_buffer_to_integer( const char *buf){
  * @return 입력 성공 여부 반환. 성공 시 SUCCESS, 실패 시 FAIL, 입력 종료 시 EXIT, 개행 문자 입력 시 AGAIN, 알 수 없는 입력 시 UNKNOWN, 프로그램 종료 시 FINISH 반환
  */
 int input_number( int *num){
-	int return_value = FAIL;	/** 입력 성공 여부를 설정할 변수 */
-	char buf[MAX_NUMS+1];		/** 입력 받을 문자열을 담을 변수 */
+	/** 입력 성공 여부를 설정할 변수 */
+	int return_value = FAIL;
+	/** 입력 받을 문자열을 담을 변수 */
+	char buf[MAX_NUMS+1];
 
 	printf("\t| @ 입력 : ");
-	if( fgets( buf, MAX_NUMS+2, stdin) == NULL){ /** 어떠한 입력도 받지 못하거나 오류가 발생하면 NULL 을 반환한다. */
+	if( fgets( buf, MAX_NUMS+2, stdin) == NULL){
+		/** 어떠한 입력도 받지 못하거나 오류가 발생하면 NULL 을 반환한다. */
 		printf("\t| ! [DEBUG] 입력 실패, fgets 함수 오류\n");
 		perror("\t| ! [DEBUG] fgets");
 		return return_value;
 	}
 	/** 입력 후 정수 또는 문자열인지 검사 */
 	/** 정수면 저장, 문자열이면 종료 코드인지 확인 */
-	//TODO
-//	while( getchar() != '\n');
 
 	return_value = check_buffer( buf);
 	switch( return_value){
-		case SUCCESS:/** 숫자를 입력 받은 경우 */
+		/** 숫자를 입력 받은 경우 */
+		case SUCCESS:
 			*num = convert_buffer_to_integer( buf);
 			if( *num >= INT_MAX) *num = FAIL;
 			break;
-		case FAIL: /** 문자열을 입력 받은 경우 */
+		/** 문자열을 입력 받은 경우 */
+		case FAIL:
 			//TODO
-			if( strncmp( buf, exit_code, MAX_NUMS) == EQUAL){ /** 입력 종료 코드(문자열)를 입력 받은 경우 */
+			/** 입력 종료 코드(문자열)를 입력 받은 경우 */
+			if( strncmp( buf, exit_code, MAX_NUMS) == EQUAL){
 				printf("\t| @ 입력 종료\n");
 				return_value = EXIT;
 			}
-			else if( strncmp( buf, finish_code, MAX_NUMS) == EQUAL){ /** 프로그램 종료 코드(문자열)를 입력 받은 경우 */
+			/** 프로그램 종료 코드(문자열)를 입력 받은 경우 */
+			else if( strncmp( buf, finish_code, MAX_NUMS) == EQUAL){
 				return_value = FINISH;
 			}
-			else{ /** 입력 종료 코드(문자열)가 아닌 알 수 없는 문자열을 입력 받은 경우 */
+			/** 입력 종료 코드(문자열)가 아닌 알 수 없는 문자열을 입력 받은 경우 */
+			else{
 				printf("\t| ! 입력 실패, 알 수 없는 입력\n");
 				return_value = UNKNOWN;
 			}
 			break;
+		/** 엔터만 입력 시 재입력 */
 		case AGAIN:
 			break;
-		default: /** return_value 가 해당 함수 로직에서 설정한 반환값이 아닌 경우 */
+		/** return_value 가 해당 함수 로직에서 설정한 반환값이 아닌 경우 */
+		default:
 			printf("\t| ! [DEBUG] check_buffer 함수에서 알 수 없는 반환값 발생\n");
 		   break;
 	}
@@ -177,10 +195,13 @@ int input_number( int *num){
  * @return 성공 시 SUCCESS, 실패 시 FAIL, 프로그램 재시작 시 AGAIN, 입력 종료 시 EXIT, 프로그램 종료 시 FINISH 반환
  */
 int input_numbers( input_data_t *odd){
-	int return_value_nums = SUCCESS; /** input_numbers 반환값 변수 */
-	int return_value_num = SUCCESS; /** input_number 반환값 변수 */
+	/** input_numbers 반환값 변수 */
+	int return_value_nums = SUCCESS;
+	/** input_number 반환값 변수 */
+	int return_value_num = SUCCESS;
 
-	if( odd == NULL){ /** 구조체 객체 포인터가 NULL 인 경우, 프로그램 종료 */
+	/** 구조체 객체 포인터가 NULL 인 경우, 프로그램 종료 */
+	if( odd == NULL){
 		printf("\t| ! [DEBUG] 입력 실패, 알 수 없는 객체 포인터 (NULL)\n");
 		return FAIL;
 	}
@@ -195,14 +216,18 @@ int input_numbers( input_data_t *odd){
 	printf("\t| @ 프로그램 종료 : end \n\n");
 
 	odd->size = UNKNOWN;
-	int number = 0; /** 임시 입력 변수 */
-	int is_loop_break = 0; /** 무한 루프 탈출 확인 변수 */
+	/** 임시 입력 변수 */
+	int number = 0;
+	/** 무한 루프 탈출 확인 변수 */
+	int is_loop_break = 0;
 
 	while( 1){
 		/** 입력 사이클 시작 */
 		if( return_value_num != AGAIN){
-			odd->size++; /** 먼저 개수를 하나 증가시켜서 최대 입력 개수를 초과하는지 확인한다. */
-			if( odd->size >= MAX_NINPUT){ /** 초과하면 입력 루틴 종료, odd->size 는 배열의 인덱스값이므로 MAX_NINPUT 값과 같으면 안된다. */
+			/** 먼저 개수를 하나 증가시켜서 최대 입력 개수를 초과하는지 확인한다. */
+			odd->size++;
+			/** 초과하면 입력 루틴 종료, odd->size 는 배열의 인덱스값이므로 MAX_NINPUT 값과 같으면 안된다. */
+			if( odd->size >= MAX_NINPUT){
 				break;
 			}
 		}
@@ -210,19 +235,26 @@ int input_numbers( input_data_t *odd){
 		return_value_num = input_number( &number);
 		switch( return_value_num){
 			/** 아래 3 가지 경우일 때 입력 루틴을 종료한다. */
-			case FAIL:		/** 입력 실패 */
-			case EXIT:		/** 입력 종료 코드 입력 */
-			case FINISH:	/** 프로그램 종료 코드 입력 */
+			/** 입력 실패 */
+			case FAIL:
+			/** 입력 종료 코드 입력 */
+			case EXIT:
+			/** 프로그램 종료 코드 입력 */
+			case FINISH:
 				is_loop_break++;
 				break;
+			/** 엔터만 입력 시 재입력 */
 			case AGAIN:
 				break;
-			case UNKNOWN:	/** 입력 종료 코드가 아닌 문자열 입력 시 재입력 진행 */
+			/** 입력 종료 코드가 아닌 문자열 입력 시 재입력 진행 */
+			case UNKNOWN:
 				continue;
-			case SUCCESS:	/** 입력 성공 시 해당 정수 저장 */
+			/** 입력 성공 시 해당 정수 저장 */
+			case SUCCESS:
 				odd->nums[odd->size] = number;
 				break;
-			default:		/** return_value 가 해당 함수 로직에서 설정한 반환값이 아닌 경우 */
+			/** return_value 가 해당 함수 로직에서 설정한 반환값이 아닌 경우 */
+			default:
 				printf("\t| ! [DEBUG] input_number 함수에서 알 수 없는 반환값 발생\n");
 				is_loop_break++;
 				break;
@@ -249,25 +281,31 @@ int input_numbers( input_data_t *odd){
  * @return 반환값 없음
  */
 void print_odd_numbers( input_data_t *odd){
-	if( odd == NULL){ /** 구조체 객체 포인터가 NULL 인 경우, 프로그램 종료 */
+	/** 구조체 객체 포인터가 NULL 인 경우, 프로그램 종료 */
+	if( odd == NULL){
 		printf("\t| ! [DEBUG] 출력 실패, 알 수 없는 객체 포인터 (NULL)\n");
 		return ;
 	}
 	
-	int loop_index = 0;	/** 반복문 인덱스 변수 */
-	int is_odd = 0;		/** 입력 받은 정수 리스트에서 홀수 개수를 카운트하는 변수 */
+	/** 반복문 인덱스 변수 */
+	int loop_index = 0;
+	/** 입력 받은 정수 리스트에서 홀수 개수를 카운트하는 변수 */
+	int is_odd = 0;
 
 	printf("\n");
-	printf("\t| @ 총 입력 개수\t: %d\n", odd->size); /** odd->size 는 현재 배열의 마지막 인덱스값이므로 개수를 구하기 위해서 1 을 더해준다. */
+	printf("\t| @ 총 입력 개수\t: %d\n", odd->size);
 	printf("\t| @ (순서)\t\t: (입력한 정수)\n");
 	for( ; loop_index <= odd->size; loop_index++){
-		if( abs( odd->nums[loop_index]) % 2 == 1){ /** 홀수만 선택. 음수 계산을 위해 절대값 사용 */
+		/** 홀수만 선택. 음수 계산을 위해 절대값 사용 */
+		if( abs( odd->nums[loop_index]) % 2 == 1){
 			printf("\t| @ %d\t\t\t: %d\n", ( loop_index + 1), odd->nums[loop_index]);
 			is_odd++;
 		}
 	}
-	if( is_odd > 0)	printf("\n\t| @ 출력 성공\n\n");	/** 홀수 카운트가 0 보다 클 경우, 홀수 출력 성공 */
-	else printf("\n\t| ! 짝수만 존재\n\n");				/** 홀수 카운트가 0 보다 작거나 같을 경우, 홀수 출력 실패 */
+	/** 홀수 카운트가 0 보다 클 경우, 홀수 출력 성공 */
+	if( is_odd > 0)	printf("\n\t| @ 출력 성공\n\n");
+	/** 홀수 카운트가 0 보다 작거나 같을 경우, 홀수 출력 실패 */
+	else printf("\n\t| ! 짝수만 존재\n\n");
 }
 
 //////////////////////////////////////
@@ -280,9 +318,11 @@ void print_odd_numbers( input_data_t *odd){
  * @return 프로그램 실행 성공 여부 반환. 성공 시 SUCCESS, 실패 시 FAIL 반환
  */
 int main(){
-	int return_value = SUCCESS; /** 함수 실행 성공 여부를 설정할 변수 */
+	/** 함수 실행 성공 여부를 설정할 변수 */
+	int return_value = SUCCESS;
 
-	input_data_t *odd = ( input_data_t*)malloc(sizeof( input_data_t)); /** 정수를 입력 받을 구조체를 할당해서 그 주소를 가지고 있는 구조체 포인터 변수 */
+	/** 정수를 입력 받을 구조체를 할당해서 그 주소를 가지고 있는 구조체 포인터 변수 */
+	input_data_t *odd = ( input_data_t*)malloc(sizeof( input_data_t));
 	if( odd == NULL){
 		printf("\t| ! [DEBUG] 객체 메모리 할당 실패, 프로그램 종료\n");
 		perror("\t| ! [DEBUG] malloc");
@@ -290,7 +330,8 @@ int main(){
 	}
 
 	while( 1){
-		return_value = input_numbers( odd); /** 입력 진행 */
+		/** 입력 진행 */
+		return_value = input_numbers( odd);
 		switch( return_value){
 			case FAIL:
 				printf("\t| ! 입력 실패, 프로그램 종료\n");
@@ -306,16 +347,16 @@ int main(){
 			case EXIT:
 				print_odd_numbers( odd);
 				break;
-			default: /** return_value 가 해당 함수 로직에서 설정한 반환값이 아닌 경우 */
+			/** return_value 가 해당 함수 로직에서 설정한 반환값이 아닌 경우 */
+			default:
 				printf("\t| ! input_data_input_numbers 함수에서 알 수 없는 반환값 발생\n");
 				break;
 		}
-		break; /** 입력 종료 */
+		break;
+		/** 입력 종료 */
 	}
 
-	free( odd); /** 할당된 구조체 변수의 메모리를 해제 */
-	//odd = NULL; /** 해제 완료 후, 해제했던 구조체 변수의 주소를 가지고 있었던 구조체 포인터 변수를 NULL 로 초기화 */
-
+	free( odd);
 	return return_value;
 }
 
